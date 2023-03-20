f90c1e1b3dab2a0f4aa50f8dbf17a72e7b8518e0
root <showell30@yahoo.com>
2023-03-20 11:08:43 +0000

    Clean up Mod5.

--- mod5.py
+++ mod5.py
@@ -31,10 +31,25 @@ class Mod5:
         return self * self.raised_to_exponent(exponent - 1)
 
 
+class Mod5Math:
+    add = lambda a, b: a + b
+    additive_inverse = lambda a: -a
+    multiply_by_constant = lambda a, b: a * b
+    power = lambda m, exp: m**exp
+    value_type = Mod5
+
+    zero = Mod5(0)
+    one = Mod5(1)
+
+
+def Mod5Poly(lst):
+    return SingleVarPoly(lst, Mod5Math, "x")
+
+
 if __name__ == "__main__":
     from commutative_ring import verify_axioms
     from single_poly import SingleVarPoly
-    from lib.test_helpers import assert_str
+    from lib.test_helpers import assert_str, run_test
 
     zero = Mod5(0)
     one = Mod5(1)
@@ -42,28 +57,17 @@ if __name__ == "__main__":
     three = Mod5(3)
     four = Mod5(4)
 
-    samples = [
-        zero,
-        one,
-        two,
-        three,
-        four,
-    ]
+    @run_test
+    def mod5_is_a_ring():
+        samples = [
+            zero,
+            one,
+            two,
+            three,
+            four,
+        ]
 
-    verify_axioms(samples, zero=zero, one=one)
-
-    class Mod5Math:
-        add = lambda a, b: a + b
-        additive_inverse = lambda a: -a
-        multiply_by_constant = lambda a, b: a * b
-        power = lambda m, exp: m**exp
-        value_type = Mod5
-
-        zero = Mod5(0)
-        one = Mod5(1)
-
-    def Mod5Poly(lst):
-        return SingleVarPoly(lst, Mod5Math, "x")
+        verify_axioms(samples, zero=zero, one=one)
 
     p_zero = Mod5Poly([])
     p_one = Mod5Poly([one])
@@ -72,15 +76,19 @@ if __name__ == "__main__":
     p_four = Mod5Poly([four])
     p_x = Mod5Poly([zero, one])
 
-    q = (p_x + p_four) * (p_x + p_three)
-    assert_str(q, "x**2+(2)*x+2")
-    assert q.eval(four) == one
+    @run_test
+    def polynomial_math_works_over_mod5():
+        q = (p_x + p_four) * (p_x + p_three)
+        assert_str(q, "x**2+(2)*x+2")
+        assert q.eval(four) == one
 
-    poly_samples = [
-        Mod5Poly([one, zero, three]),
-        Mod5Poly([three, one, four, two]),
-        Mod5Poly([two]),
-        (p_x * p_two) + p_one,
-        (p_x + p_three).raised_to_exponent(15),
-    ]
-    verify_axioms(poly_samples, zero=p_zero, one=p_one)
+    @run_test
+    def mod5_polys_conform_to_axioms():
+        poly_samples = [
+            Mod5Poly([one, zero, three]),
+            Mod5Poly([three, one, four, two]),
+            Mod5Poly([two]),
+            (p_x * p_two) + p_one,
+            (p_x + p_three).raised_to_exponent(15),
+        ]
+        verify_axioms(poly_samples, zero=p_zero, one=p_one)
