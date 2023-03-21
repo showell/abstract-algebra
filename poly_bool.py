83f88c1adba9891d46d428368ce6acbefb8fad7c
root <showell30@yahoo.com>
2023-03-21 14:37:31 +0000

    Show bool powers.

--- poly_bool.py
+++ poly_bool.py
@@ -20,6 +20,7 @@ class BoolPoly:
     const = lambda c: SingleVarPoly.constant(BoolMath, Bool(c))
     zero = const(False)
     one = const(True)
+    p = SingleVarPoly.degree_one_var(BoolMath, "p")
 
     @staticmethod
     def from_list(lst):
@@ -53,6 +54,12 @@ if __name__ == "__main__":
         x = BoolPoly.from_ints({0, 2, 4, 6})
         y = BoolPoly.from_ints({3})
         z = BoolPoly.from_ints({4})
+
+        b = BoolPoly.p
+        assert x == b**0 + b**2 + b**4 + b**6
+        assert y == b**3
+        assert z == b**4
+
         p = x * y * z
         assert_str(p, "p**13+p**11+p**9+p**7")
         assert BoolPoly.to_ints(p) == [7, 9, 11, 13]
@@ -65,5 +72,10 @@ if __name__ == "__main__":
         assert_str(q, "p**6+p**4+p**3+p**2+True")
         assert BoolPoly.to_ints(q) == [0, 2, 3, 4, 6]
 
+        # Evaluating boolean polynomials is kind of silly, since
+        # we are only really looking at the "constant" coefficient.
         assert p.eval(F) == F
         assert p.eval(T) == T
+
+        assert q.eval(F) == T
+        assert q.eval(T) == T
