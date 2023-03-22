74552a07806a2194c4354ed09ae9d151c61c7734
root <showell30@yahoo.com>
2023-03-22 14:50:54 +0000

    comments

--- mapping.py
+++ mapping.py
@@ -1,9 +1,8 @@
 def verify_mappability(samples, f, g, type1, type2):
-    # https://en.wikipedia.org/wiki/Homomorphism
     # https://en.wikipedia.org/wiki/Surjective_function
     # https://en.wikipedia.org/wiki/Injective_function
     """
-    We are using a somewhat informal notion of homomorphism here,
+    We are using a somewhat informal notion of mappability here,
     which is essentially just saying that two different Python
     types have a natural mapping between them.
 
@@ -27,6 +26,12 @@ def verify_mappability(samples, f, g, type1, type2):
     the same as if you had done the same operation completely in
     type1.
 
+    Under this scheme, g is a homomorphism from type2 to type1,
+    but f may not necessarily be one. (Another caveat is that g
+    may only make sense for a subset of type2 values, although
+    I think most, if not all, of the examples here are
+    complete.)
+
     Unless we are dealing with small, finite sets, this function
     does not exhaust all possible values, so you shouldn't
     consider this function to provide a proof. It is useful
@@ -43,11 +48,36 @@ def verify_mappability(samples, f, g, type1, type2):
             assert g(f(a) ** exp) == a**exp
 
         for b in samples:
+            # show that we can compute values in type2
             assert g(f(a) + f(b)) == a + b
             assert g(f(a) * f(b)) == a * b
 
+            # https://en.wikipedia.org/wiki/Homomorphism
+            # show g is a homomorpism, at least for the image of
+            # our samples over f
+            x = f(a)
+            y = f(b)
+            assert g(x + y) == g(x) + g(y)
+            assert g(x * y) == g(x) * g(y)
+
 
 def verify_isomorphism(*, samples_a, type_a, samples_b, type_b, a_to_b, b_to_a):
+    # https://en.wikipedia.org/wiki/Isomorphism
+    """
+    In math, the term "isomorphism" if a fancy and more precise way of
+    saying that two different things are really equivalent things, for all
+    practical purposes.  In other words, two things may look different on a
+    superficial level, but they have the same underlying structure.
+
+    In order to show that two different Python data types are isomorphic
+    (at least over the basic operations of addition and multiplication),
+    it is sufficient to prove that there is a pair of functions that map
+    between the two types in such a way that you can choose either type
+    to do the actual multiplication/addition computations, and then the
+    other type essentially gets the result for free.  (You apply a forward
+    mapping to the inputs, then the treat the other Python type as a black
+    box, and then you apply the reverse mapping to the output.)
+    """
     verify_mappability(samples_a, a_to_b, b_to_a, type_a, type_b)
     verify_mappability(samples_b, b_to_a, a_to_b, type_b, type_a)
 
@@ -140,6 +170,23 @@ if __name__ == "__main__":
 
     @run_test
     def NumberList_and_IntegerPoly_are_isomorphic():
+        """
+        IntegerPoly is a more interesting class than NumberList,
+        because IntegerPoly has the notion of variables, and you can
+        actually plug in variables to evaluate the polynomials.
+
+        On the other hand, the two classes behave identically with
+        respect to addition and multiplication, so they are isomorphic
+        over those operations.
+
+        In fact, we could have built IntegerPoly on top of NumberList.
+        In this project I didn't follow that strategy, but if you look
+        at the code, there are striking similarities between the two
+        data types, and this is no coincidence.
+
+        Likewise, we could easily re-implement NumberList to just be
+        a simple wrapper around IntegerPoly.
+        """
         samples_a = [
             IntegerPoly.from_list([0, 1, 3]),
             IntegerPoly.from_list([-47]),
